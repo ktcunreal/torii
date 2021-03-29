b41d2c1c67dbffe274146f4ea1525063bfccba97
ktcunreal <ktcunreal@gmail.com>
2021-03-29 16:37:21 +0800

    slighty modify

--- utils/socks5.go
+++ utils/socks5.go
@@ -10,10 +10,9 @@ import (
 
 type Socks5Server struct {
 	net.Conn
-	res, buf  []byte
-	dom, addr string
-	ip        net.IP
-	port      uint16
+	res, buf []byte
+	addr     string
+	ip       net.IP
 }
 
 func NewSocks5Server(conn net.Conn) *Socks5Server {
@@ -25,65 +24,61 @@ func NewSocks5Server(conn net.Conn) *Socks5Server {
 }
 
 func (s *Socks5Server) Proxy() error {
-	if n, err := io.ReadFull(s.Conn, s.buf[:3]); err != nil || n != 3 {
+	if _, err := io.ReadFull(s.Conn, s.buf[:3]); err != nil {
 		return s.Conn.Close()
 	}
 
-	if n, err := s.Conn.Write(s.res[:2]); n != 2 || err != nil {
+	if _, err := s.Conn.Write(s.res[:2]); err != nil {
 		return s.Conn.Close()
 	}
 
-	if n, err := io.ReadFull(s.Conn, s.buf[:4]); n != 4 || err != nil {
+	if _, err := io.ReadFull(s.Conn, s.buf[:4]); err != nil {
 		return s.Conn.Close()
 	}
 
 	switch s.buf[3] {
-	case 0x01:
-		if n, err := io.ReadFull(s.Conn, s.buf[:6]); n != 6 || err != nil {
-			return s.Conn.Close()
-		}
-		s.ip, s.port = s.buf[:4], binary.BigEndian.Uint16(s.buf[4:6])
-		s.addr = fmt.Sprintf("%s:%d", s.ip, s.port)
 
 	case 0x03:
-		if n, err := io.ReadFull(s.Conn, s.buf[:1]); n != 1 || err != nil {
+		if _, err := io.ReadFull(s.Conn, s.buf[:1]); err != nil {
 			return s.Conn.Close()
 		}
-		l := int(s.buf[0])
-		b := make([]byte, l+2)
-		if n, err := io.ReadFull(s.Conn, b); n != l+2 || err != nil {
+
+		dom_buf := make([]byte, int(s.buf[0]))
+
+		if _, err := io.ReadFull(s.Conn, dom_buf); err != nil {
 			return s.Conn.Close()
 		}
-		s.dom, s.port = string(b[:l]), binary.BigEndian.Uint16(b[l:])
-		s.addr = fmt.Sprintf("%s:%d", s.dom, s.port)
+
+		if _, err := io.ReadFull(s.Conn, s.buf[6:]); err != nil {
+			return s.Conn.Close()
+		}
+
+		s.addr = fmt.Sprintf("%s:%d", string(dom_buf), binary.BigEndian.Uint16(s.buf[6:]))
 
 	default:
 		return s.Conn.Close()
 	}
 
-	dst, err := net.DialTimeout("tcp", s.addr, time.Second*10)
+	dst, err := net.DialTimeout("tcp", s.addr, time.Second*5)
 	if err != nil {
 		return s.Conn.Close()
 	}
 
-	if n, err := s.Conn.Write(s.res); n != 10 || err != nil {
+	if _, err := s.Conn.Write(s.res); err != nil {
 		return s.Conn.Close()
 	}
 
-	Pipe(s.Conn, dst)
+	go Pipe(s.Conn, dst)
 	return nil
 }
 
 func Pipe(src, dst net.Conn) {
-	go func() {
-		defer src.Close()
-		defer dst.Close()
-		io.Copy(src, dst)
-	}()
-	go func() {
-		defer src.Close()
-		defer dst.Close()
-		io.Copy(dst, src)
-	}()
+	copyConn := func(a, b net.Conn) {
+		defer a.Close()
+		defer b.Close()
+		io.Copy(a, b)
+		return
+	}
+	go copyConn(src, dst)
+	go copyConn(dst, src)
 }
-
