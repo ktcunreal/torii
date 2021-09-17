8630278b8d59299056bcac3046ed67e264ad48e4
ktcunreal <ktcunreal@gmail.com>
2021-09-17 17:52:28 +0800

    Add timestamp validation

--- utils/encrypt.go
+++ utils/encrypt.go
@@ -10,22 +10,28 @@ import (
 	"io"
 	"log"
 	"net"
+	"time"
 )
 
-const Chunk int = 16384
+const (
+	Chunk    int = 16384
+	ValidRng int = 180
+)
 
 type Key struct {
-	hash   *[32]byte
-	salt   []byte
-	pepper []byte
+	hash     *[32]byte
+	salt     []byte
+	pepper   []byte
+	cinnamon []byte
 }
 
 func NewKey(s string) *Key {
 	h := SH256R(s)
 	return &Key{
-		hash:   &h,
-		salt:   SH256L(h[:12]),
-		pepper: SH256L(h[20:]),
+		hash:     &h,
+		salt:     SH256L(h[:12]),
+		pepper:   SH256L(h[4:16]),
+		cinnamon: SH256L(h[8:20]),
 	}
 }
 
@@ -40,7 +46,7 @@ func NewEncStream(conn net.Conn, psk *Key) *EncStream {
 	return &EncStream{
 		Conn: conn,
 		key:  psk,
-		dBuf: make([]byte, 12),
+		dBuf: make([]byte, 16),
 	}
 }
 
@@ -51,11 +57,11 @@ func (e *EncStream) Read(b []byte) (int, error) {
 		return n, nil
 	}
 
-	if n, err := io.ReadFull(e.Conn, e.dBuf); err != nil || n != 12 {
+	if n, err := io.ReadFull(e.Conn, e.dBuf); err != nil || n != 16 {
 		return 0, err
 	}
 
-	size, ok := Decode(e.dBuf, e.key.salt, e.key.pepper)
+	size, ok := Decode(e.dBuf, e.key.salt, e.key.pepper, e.key.cinnamon)
 	if !ok {
 		log.Println("INVALID CIPHER LENGTH")
 		return e.Drop()
@@ -92,7 +98,7 @@ func (e *EncStream) Write(b []byte) (int, error) {
 		}
 		cipher := secretbox.Seal([]byte{}, b[sidx:eidx], &e.sNonce, e.key.hash)
 		increment(&e.sNonce)
-		enc := Encode(len(cipher), e.key.salt, e.key.pepper)
+		enc := Encode(len(cipher), e.key.salt, e.key.pepper, e.key.cinnamon)
 		if _, err := e.Conn.Write(enc); err != nil {
 			return sidx, err
 		}
@@ -109,7 +115,7 @@ func (e *EncStream) Close() error {
 
 func (e *EncStream) Drop() (int, error) {
 	defer e.Conn.Close()
-	trap := make([]byte, 12)
+	trap := make([]byte, 16)
 	for {
 		_, err := io.ReadFull(e.Conn, trap)
 		if err != nil {
@@ -119,28 +125,49 @@ func (e *EncStream) Drop() (int, error) {
 	return 0, errors.New("ILLEGAL CONNECTION")
 }
 
-func Encode(i int, salt, pepper []byte) []byte {
-	iBuf, head, hBuf := make([]byte, 4), make([]byte, 12), make([]byte, 36)
-	binary.LittleEndian.PutUint32(iBuf, uint32(i))
+func Encode(i int, salt, pepper, cinnamon []byte) []byte {
+	iBuf := make([]byte, 4)
+	head := make([]byte, 16)
+	hBuf := make([]byte, 36)
+
 	rand.Read(head[:4])
+
 	copy(hBuf[:4], head[:4])
 	copy(hBuf[4:], salt)
 	copy(head[4:8], SH256S(hBuf))
+
+	t := time.Now().Unix()
+	binary.LittleEndian.PutUint32(iBuf, uint32(t))
+	copy(hBuf[4:], cinnamon)
+	copy(head[8:12], XORBytes(iBuf, SH256S(hBuf)))
+
+	binary.LittleEndian.PutUint32(iBuf, uint32(i))
 	copy(hBuf[4:], pepper)
-	copy(head[8:], XORBytes(iBuf, SH256S(hBuf)))
+	copy(head[12:], XORBytes(iBuf, SH256S(hBuf)))
+
 	return head
 }
 
-func Decode(b []byte, salt, pepper []byte) (int, bool) {
+func Decode(b, salt, pepper, cinnamon []byte) (int, bool) {
 	hBuf := make([]byte, 36)
+
 	copy(hBuf[:4], b[:4])
 	copy(hBuf[4:], salt)
-	if !bytes.Equal(SH256S(hBuf), b[4:8]) {
+	if !bytes.Equal(b[4:8], SH256S(hBuf)) {
 		return 0, false
 	}
-	copy(hBuf[4:], pepper)
+
+	copy(hBuf[4:], cinnamon)
 	iBuf := XORBytes(b[8:12], SH256S(hBuf))
+	if Abs(int(time.Now().Unix())-int(binary.LittleEndian.Uint32(iBuf))) > ValidRng {
+		log.Println("EXPIRED TIMESTAMP")
+		return 0, false
+	}
+
+	copy(hBuf[4:], pepper)
+	iBuf = XORBytes(b[12:16], SH256S(hBuf))
 	i := int(binary.LittleEndian.Uint32(iBuf))
+
 	return i, true
 }
 
@@ -173,4 +200,11 @@ func SH256S(b []byte) []byte {
 
 func SH256R(s string) [32]byte {
 	return sha256.Sum256([]byte(s))
+}
+
+func Abs(i int) int {
+	if i < 0 {
+		return -i
+	}
+	return i
 }
