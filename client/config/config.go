a0cc93aff3b1cfca6cc8eb28d73403fcba60a171
ktcunreal <ktcunreal@protonmail.com>
2022-07-01 09:58:00 +0800

    Update config.go
    Fixed validateDomainPort

--- client/config/config.go
+++ client/config/config.go
@@ -105,7 +105,7 @@ func validateIP(s string) bool {
 
 func validateDomain(s string) bool {
 	RegExp := regexp.MustCompile(`^(([a-zA-Z]{1})|([a-zA-Z]{1}[a-zA-Z]{1})|([a-zA-Z]{1}[0-9]{1})|([0-9]{1}[a-zA-Z]{1})|([a-zA-Z0-9][a-zA-Z0-9-_]{1,61}[a-zA-Z0-9]))\.([a-zA-Z]{2,6}|[a-zA-Z0-9-]{2,30}\.[a-zA-Z
- ]{2,3})$`)
+ ]{2,3}):\d{1,5}$`)
 	if RegExp.MatchString(s) {
 		return true
 	}
